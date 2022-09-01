97266e096912f137486700f72554dd8ae1cfee11
nafishahmeddev <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>
2022-09-01 23:34:04 +0530

    update

--- resources/views/admin/event/listing.blade.php
+++ resources/views/admin/event/listing.blade.php
@@ -2,30 +2,47 @@
 
 
 <div class="container py-2">
-    <table class="table">
-        <thead>
-            <tr>
+    <h2>Events</h2>
 
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($events as $event)
-            <tr>
-                <td>{{$event->name}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <nav aria-label="Page navigation example">
-        <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            @for($pageNo = 1; $pageNo <= $events->lastPage(); $pageNo++)
-            <li class="page-item {{ $events->currentPage() == $pageNo?'active':''}}"><a class="page-link" href="#">{{$pageNo}}</a></li>
-            @endfor
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-        </ul>
-    </nav>
-    {{dd($events)}}
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Entry type</th>
+                    <th>Venue</th>
+                    <th>City</th>
+                    <th>Address</th>
+                    <th>Start</th>
+                    <th>End</th>
+                    <th>Occurrence</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($events as $index=>$event)
+                <tr>
+                    <td>{{(($events->currentPage() - 1)* $events->perPage()) +  $index + 1 }}</td>
+                    <td>{{$event->name}}</td>
+                    <td>{{$event->entry_type}}</td>
+                    <td>{{$event->venue}}</td>
+                    <td>{{$event->city}}</td>
+                    <td>{{$event->address}}</td>
+                    <td>{{$event->start_date}}</td>
+                    <td>{{$event->end_data}}</td>
+                    <td>{{$event->occurrence}}</td>
+                    <td class="text-end">
+                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+   
+    <div class="mt-2 text-end">
+        @include('admin.common.pagination', ['paginator' => $events])
+    </div>
 </div>
 
 @include("admin.common.footer")
