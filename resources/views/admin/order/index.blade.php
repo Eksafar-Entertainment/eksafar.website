c117a00f786004f1fe57eb35a913ebf3d273cc8c
nafishahmeddev <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>
2022-09-05 21:22:30 +0530

    fx

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="rounded">
-    <h3>Posts</h3>
+    <h3>Orders</h3>
     <p class="text-muted">Manage your order here.</p>
     <div class="mt-2">
         @include('layouts.partials.messages')
@@ -13,16 +13,30 @@
     <table class="table table-bordered table-striped">
         <thead>
             <tr>
-                <th width="1%">No</th>
+                <th width="1%">#</th>
+                <th width="1%">ID</th>
+
                 <th>Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+                <th>Amount</th>
+                <th>Status</th>
+                <th>Checked In</th>
                 <th width="3%" colspan="3">Action</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($orders as $key => $order)
             <tr>
+                <td>{{ $key }}</td>
                 <td>{{ $order->id }}</td>
-                <td>{{ $order->title }}</td>
+                <td>{{ $order->name }}</td>
+                <td>{{ $order->mobile }}</td>
+                <td>{{ $order->email }}</td>
+                <td>{{ $order->total_price }}</td>
+                <td>{{ $order->payment_status }}</td>
+                <td>{{ $order->is_checked_in }}</td>
+
                 <td>
                     <a class="btn btn-info btn-sm" href="{{ route('order.show', $order->id) }}">Show</a>
                 </td>
