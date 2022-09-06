2fa3fec283864e2c7006dbe80b036efe52955188
nafishahmeddev <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>
2022-09-06 10:30:12 +0530

    fixup

--- database/seeders/EventTicketsTableSeeder.php
+++ database/seeders/EventTicketsTableSeeder.php
@@ -16,27 +16,54 @@ class EventTicketsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('events')->insert([
+            [
+                'name' => "Disco Dandiya Night",
+                'entry_type'=>"Normal",
+                'venue'=>"Catch Up",
+                'city'=>"Bangalore",
+                'address'=> "OK",
+                'start_date'=>"2022-09-15",
+                'end_date'=>"2022-09-15",
+                'occurrence'=>"",
+                'description'=>"",
+                'cover_image'=>"",
+                'video_link'=>"",
+                'event_type'=>"",
+                'artist'=>"",
+                'abilities'=>"",
+                'slug'=>""
+            ],
+        ]);
         DB::table('event_tickets')->insert([
-            ['event_id' => 1,
-            'name' => 'Female',
-            'price' => 299,
-            'persons' => 0,
-            'description' => 'tickets for Female'],
-            ['event_id' => 1,
-            'name' => 'Male',
-            'price' => 399,
-            'persons' => 0,
-            'description' => 'tickets for Male'],
-            ['event_id' => 1,
-            'name' => 'Couple',
-            'price' => 599,
-            'persons' => 0,
-            'description' => 'tickets for Couples'],
-            ['event_id' => 1,
-            'name' => 'Kids',
-            'price' => 99,
-            'persons' => 0,
-            'description' => 'tickets for Kids'],
+            [
+                'event_id' => 1,
+                'name' => 'Female',
+                'price' => 299,
+                'persons' => 0,
+                'description' => 'tickets for Female'
+            ],
+            [
+                'event_id' => 1,
+                'name' => 'Male',
+                'price' => 399,
+                'persons' => 0,
+                'description' => 'tickets for Male'
+            ],
+            [
+                'event_id' => 1,
+                'name' => 'Couple',
+                'price' => 599,
+                'persons' => 0,
+                'description' => 'tickets for Couples'
+            ],
+            [
+                'event_id' => 1,
+                'name' => 'Kids',
+                'price' => 99,
+                'persons' => 0,
+                'description' => 'tickets for Kids'
+            ],
         ]);
     }
 }
