745060d2b946c59949e7c318b3c9c6172b867fff
nafishahmeddev <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>
2022-09-11 01:35:17 +0530

    fx

--- resources/views/frontend/landing/gallery.blade.php
+++ resources/views/frontend/landing/gallery.blade.php
@@ -16,7 +16,7 @@
         @foreach($gallery as $gallery_image)
         <div class="col-md-4">
             <div>
-                <div style="background-size:cover; background-image: url({{$gallery_image->url}}); padding-top: 100%; background-position:center; box-shadow: 0 0 10px rgba(0,0,0,0.09); border-radius: 8px">
+                <div style="background-size:cover; background-image: url({{$gallery_image->url}}); padding-top: 100%; background-position:center; box-shadow: 0 0 10px rgba(0,0,0,0.09);">
 
                 </div>
             </div>
