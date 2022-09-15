8230d1e19f9d8b38d9e07c7da0f223ae1a7d4bed
nafishahmeddev <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>
2022-09-15 20:21:08 +0530

    fix

--- resources/views/admin/event/listing.blade.php
+++ resources/views/admin/event/listing.blade.php
@@ -1,53 +1,60 @@
 @extends('admin.layouts.admin')
 @section('content')
-<div class="container py-2">
-    <div class="card">
-        <div class="card-header d-flex">
-            <h4 class="header-title flex-grow-1">{{__('Events')}}</h4>
-            <div class="">
-                <a href="{{url('admin/event/form')}}" class="btn btn-sm btn-primary">New Event</a>
-            </div>
+<div class="container">
+
+    <div class="d-flex">
+        <h4 class="flex-grow-1">{{__('Events')}}</h4>
+        <div class="">
+            <a href="{{url('admin/event/form')}}" class="btn btn-sm btn-primary">New Event</a>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Entry type</th>
-                            <th scope="col">Venue</th>
-                            <th scope="col">City</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Start</th>
-                            <th scope="col">End</th>
-                            <th scope="col">Occurrence</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($events as $index=>$event)
-                        <tr>
-                            <td>{{(($events->currentPage() - 1)* $events->perPage()) +  $index + 1 }}</td>
-                            <td>{{$event->name}}</td>
-                            <td>{{$event->entry_type}}</td>
-                            <td>{{$event->venue}}</td>
-                            <td>{{$event->city}}</td>
-                            <td>{{$event->address}}</td>
-                            <td>{{$event->start_date}}</td>
-                            <td>{{$event->end_data}}</td>
-                            <td>{{$event->occurrence}}</td>
-                            <td class="text-end">
-                                <a type="button" class="btn btn-sm btn-danger" onclick="if(confirm('Are you sure')){window.location.href=`{{url('admin/event/delete/'.$event->id)}}`}">Delete</a>
-                                <a type="button" class="btn btn-sm btn-primary" href="{{url('/admin/event/form/'.$event->id)}}">Edit</a>
-                                <a class="btn btn-sm btn-deafult" href="{{url('/admin/event/'.$event->id."/dashboard")}}">Manage</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    </div>
+
+
+    <div class="row mt-4">
+        @foreach($events as $index=>$event)
+        <div class="col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header bg-primary text-light d-flex align-items-center">
+                    <div class="bg-white text-primary text-center p-1 border-primary border" style="width: 50px; height: 60px; margin-top: -20px">
+                        <small>{{ \Carbon\Carbon::parse($event->start_date)->format('M')}}</small>
+                        <span class="fs-5">{{ \Carbon\Carbon::parse($event->start_date)->format('d')}}</span>
+                    </div>
+                    <div class="flex-grow-1 ps-3">
+                        <h5 class="mb-0">{{$event->name}}</h5>
+                        <p class="mb-0">At {{$event->venue}}</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col text-center">
+                            <span class="fs-5">0</span><br />
+                            <span class="text-primary">Tickets Sold</span>
+                        </div>
+
+                        <div class="col text-center">
+                            <span class="fs-5">
+                                @money(20000)
+                            </span><br />
+                            <span class="text-primary">Revenue</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer d-flex">
+                    <button class="btn btn-light btn-sm flex-grow-1" onclick="if(confirm('Are you sure')){window.location.href=`{{url('admin/event/delete/'.$event->id)}}`}">
+                        <i class="fas fa-trash"></i> Delete
+                    </button>
+                    <a class="btn btn-light btn-sm flex-grow-1" href="{{url('/admin/event/form/'.$event->id)}}">
+                        <i class="fas fa-pencil"></i> Edit
+                    </a>
+                    <a class="btn btn-light btn-sm flex-grow-1" href="{{url('/admin/event/'.$event->id."/dashboard")}}">
+                        <i class="fas fa-settings"></i> Manage
+                    </a>
+                </div>
             </div>
+
         </div>
+        @endforeach
+
     </div>
 
     <div class="mt-2 text-end">
