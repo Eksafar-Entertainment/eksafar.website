fe993d7f0dceb7df8f4ba3239a53fb205c6b217e
nafishahmeddev <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>
2022-09-19 00:23:58 +0530

    [FEATURE] Lots of features

--- app/Http/Controllers/Admin/PermissionsController.php
+++ app/Http/Controllers/Admin/PermissionsController.php
@@ -16,7 +16,7 @@ class PermissionsController extends Controller
      */
     public function index()
     {   
-        $permissions = Permission::all();
+        $permissions = Permission::orderBy("name")->get();
         return view('admin.permissions.index', [
             'permissions' => $permissions
         ]);
