7c7b7ab3bc54b21544fded84921a431842f6f739
nafishahmeddev <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>
2022-09-19 01:45:22 +0530

    [FEATURE] Lots of features

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -164,7 +164,7 @@ class EventController extends Controller
         {
             return '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
         }
-        $colors = ["red", "green", "blue", "orange", "indigo", "purple", "brown"];
+        $colors = ["#FB8C00", "#00C853", "#00BCD4", "#ff5722", "#6200ea", "#aa00ff", "#3e2723"];
         foreach ($event_tickets as $i => $event_ticket) {
             $color = $colors[$i];
             $tickets_sold_details_chart["datasets"][$event_ticket->id] = [
