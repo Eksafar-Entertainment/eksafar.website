4258a1cfac489b67882df822984276321e556a02
nafishahmeddev <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>
2022-09-23 22:06:16 +0530

    [UPDATE] fix

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -77,6 +77,47 @@
         <main>
             @yield('content')
         </main>
+
+        <footer id="footer" class="bg-primary text-light pt-5">
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <h4>Useful Links</h4>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a class="text-light" href="#">Home</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">About us</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Services</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Terms of service</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Privacy policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <h4>Our Services</h4>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a class="text-light" href="#">Web Design</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Web Development</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Product Management</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Marketing</a></li>
+                                <li class="nav-item"><a class="text-light" href="#">Graphic Design</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-contact">
+                            <h4>Contact Us</h4>
+                            <p> A108 Adam Street <br> New York, NY 535022<br> United States <br><br> <strong>Phone:</strong> +1 5589 55488 55<br> <strong>Email:</strong> info@example.com<br> </p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-info">
+                            <h3>About DevVE</h3>
+                            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-3">
+                <div class="copyright"> &copy; Copyright <strong><span>DevVE</span></strong>. All Rights Reserved </div>
+                <div class="credits"> Designed by <a class="text-light" href="#">SalvadorDevVE</a> </div>
+            </div>
+        </footer>
     </div>
 </body>
 
