05c7c86979818191adc4fc14a7856aec2fc1af3c
nafishahmeddev <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>
2022-09-23 20:08:50 +0530

    [UPDATE] fix

--- resources/views/frontend/event/details.blade.php
+++ resources/views/frontend/event/details.blade.php
@@ -1,21 +1,104 @@
 @extends('frontend.layouts.default')
 
 @section('content')
-<div class="container-lg">
+<section class="py-4 pt-0">
+    <div class="container">
+        <div class="row gx-5">
+            <div class="col-md-8">
+                <div class="pt-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/1080/580" class="" style="width: 100%;" />
+                    </div>
 
-    <div class="row">
-        <div class="col-md-8">
-            <div class="" style="height: 1500px;"></div>
-        </div>
 
-        <div class="col-md-4">
+                    <div class="mt-5">
+                        <h4>Description</h4>
+                        <p>Catch the 'O Sanam' hitmaker Lucky Ali perform live at the SteppinOut Indie Fest along with the best of Indie Artists pan India. With Dikshant who is the soulful voice behind the popular song "Tum Akhon Se Batana", Lost Stories - one of the biggest name in Dance Music in India and South Asia, Jaden Maskie Goa-Based Singer-Songwriter who wears many hats as an artist, apart from writing and producing his own music, the singer-songwriter also mixes and masters his tunes. Micah Bedford is an upcoming Hip- Hop/R&B artist who's been making waves since March, 2020. Arham Fulfagar is a singer-songwriter originally from Guwahati, Assam currently based out of Mumbai and has started releasing his songs this year. His music stems out of his personal experiences and he is inspired by artists like Damien Rice, Ray LaMontagne Leonard Cohen, Bharat Chauhan, Prateek Kuhad and more.</p>
+                        <p>Soak in the beauty of musical transcendence and immerse yourself in the joy of pure music. The SteppinOut Indie fest is all set to be a mixed bag of entertainment with music, food, and an interesting line-up of budding indie artists who’d take you on a journey to your soul.</p>
+                        <p>From unlimited jam sessions, enriching musical banter, groovy tunes, soulful melody, and all that jazz- The Indie fest is about to be epic. So get ready to meet your favorite artists, or simply find one.</p>
 
-            <div class="card sticky-top mt-4">
-                <div class="p-4">
-                    <h4>Disco Dandiya Night</h4>
-                    <div class="px-4 py-2 d-flex">
-                        <div><i class="fas fa-bookmark"></i> </div>
-                        <div class="flex-grow-1"></div>
+                        <br />
+                        <br />
+                        <h4>Instructions</h4>
+                        <ul>
+                            <li>Alcohol will be served to audiences above 21 years of age.</li>
+                            <li>Alcohol will be served to audiences above 21 years of age.</li>
+                            <li>Alcohol will be served to audiences above 21 years of age.</li>
+                        </ul>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="sticky-top pt-4">
+                    <div class="card">
+                        <div class="p-4 pb-3">
+                            <div class="d-flex">
+                                <h4 class="flex-grow-1">Disco Dandiya Night | Navratri 2022</h4>
+                                <div>
+                                    <button class="btn btn-sm btn-outline-primary"><i class="fas fa-heart"></i> </button>
+                                </div>
+                            </div>
+                            <div class="pb-2 pt-2 d-flex">
+                                <div><i class="fas fa-bookmark"></i></div>
+                                <div class="flex-grow-1 ps-3">Music</div>
+                            </div>
+
+                            <div class="pb-2 d-flex">
+                                <div><i class="fas fa-calendar"></i></div>
+                                <div class="flex-grow-1 ps-3">September 24 | 5PM</div>
+                            </div>
+
+                            <div class="d-flex">
+                                <div><i class="fa-solid fa-location-dot"></i></div>
+                                <div class="flex-grow-1 ps-3">Phoenix Marketcity, Bengaluru</div>
+                            </div>
+                        </div>
+
+                        <div class="px-4 py-2 bg-light text-info">
+                            <div class="d-flex">
+                                <div><i class="fa-solid fa-shoe-prints"></i></div>
+                                <div class="flex-grow-1 ps-3">Step out and enjoy this event</div>
+                            </div>
+                        </div>
+
+                        <div class="p-4 pt-3 d-flex">
+                            <div class="fs-4"><i class="fas fa-wallet"></i> </div>
+                            <div class="flex-grow-1 ps-3 fs-4 fw-bold">Rs 3000</div>
+                            <div>
+                                <button class="btn btn-danger">Book Now</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="card mt-4 card-body">
+                        <h5>Event Guide</h5>
+
+                        <hr/>
+                        <div class="d-flex">
+                            <div class="fs-3 text-secondary"><i class="fa-solid fa-person-walking"></i></div>
+                            <div class="flex-grow-1 ps-4">
+                                <small class="text-muted mb-0 d-flex">For Age</small>
+                                <span>15+</span>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-3">
+                            <div class="fs-3 text-secondary"><i class="fa-solid fa-globe"></i></div>
+                            <div class="flex-grow-1 ps-4">
+                                <small class="text-muted mb-0 d-flex">Language</small>
+                                <span>Hinglish</span>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-3">
+                            <div class="fs-3 text-secondary"><i class="fa-solid fa-microphone-lines"></i></div>
+                            <div class="flex-grow-1 ps-4">
+                                <small class="text-muted mb-0 d-flex">Live Performance</small>
+                                <span>Enjoy a unique experience</span>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
@@ -23,9 +106,23 @@
             </div>
 
         </div>
-
     </div>
+</section>
 
-</div>
-
+<section class="py-5 bg-secondary text-light">
+    <div class="container my-4">
+        <div class="row align-items-center gx-5 gy-5">
+            <div class="col-md-6">
+                <h2>No More Screens. <br />Only LIVE Scenes.</h2>
+                <h4>Kiss the couch goodbye and make a checklist of the things you’ve missed! Concerts. Comedy. Cricket. Camping. Cool Scenes.</h4>
+                <h4>Set your destination to: ‘Anywhere, but home.’ Find experiences in & around your city - Step out with the Paytm Insider app today.</h4>
+            </div>
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-4">
+                <img src="https://media.insider.in/image/upload/c_crop,g_custom/v1637927732/pkle4f1xuhlf5kfwlcvg.gif" style="width: 100%;" />
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,13 +13,12 @@
     <!-- Scripts -->
     <script src="{{ asset('js/front/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/front/app.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -40,35 +40,34 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -80,4 +79,5 @@
         </main>
     </div>
 </body>
+
 </html>
