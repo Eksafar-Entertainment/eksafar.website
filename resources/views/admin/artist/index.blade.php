d3b43bef410f9155d0076a3b86607d5f84411e93
Ravi Sharma <rsravicoder38@gmail.com>
2022-09-25 14:10:57 +0530

    status

--- resources/views/admin/artist/index.blade.php
+++ resources/views/admin/artist/index.blade.php
@@ -4,10 +4,10 @@
 
 
 <div class="bg-light p-4 rounded">
-    <h2>Venues</h2>
+    <h2>Artist</h2>
     <div class="lead">
-        Manage your venue here.
-        <a href="{{ route('venue.create') }}" class="btn btn-primary btn-sm float-right">Add venue</a>
+        Manage your Artist here.
+        <a href="{{ route('artist.create') }}" class="btn btn-primary btn-sm float-right">Add artist</a>
     </div>
 
     <div class="mt-2">
@@ -27,13 +27,13 @@
             <td>{{ $artist->cover }}</td>
             <td>{{ $artist->name }}</td>
             {{-- <td>
-                <a class="btn btn-info btn-sm" href="{{ route('venue.show', $venue->id) }}">Show</a>
+                <a class="btn btn-info btn-sm" href="{{ route('artist.show', $artist->id) }}">Show</a>
             </td> --}}
             <td>
-                <a class="btn btn-primary btn-sm" href="{{ route('venue.edit', $venue->id) }}">Edit</a>
+                <a class="btn btn-primary btn-sm" href="{{ route('artist.edit', $artist->id) }}">Edit</a>
             </td>
             <td>
-                {!! Form::open(['method' => 'DELETE','route' => ['venue.destroy', $venue->id],'style'=>'display:inline']) !!}
+                {!! Form::open(['method' => 'DELETE','route' => ['artist.destroy', $artist->id],'style'=>'display:inline']) !!}
                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                 {!! Form::close() !!}
             </td>
@@ -47,3 +47,5 @@
 
 </div>
 @endsection
+
+
