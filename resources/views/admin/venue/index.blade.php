4501fa5dfe399e67feb946cef0d3f6318e2850bc
nafishahmeddev <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>
2022-09-25 20:40:12 +0530

    [UPDATE] details page layout fix

--- resources/views/admin/venue/index.blade.php
+++ resources/views/admin/venue/index.blade.php
@@ -1,49 +1,54 @@
 @extends('admin.layouts.admin')
 
 @section('content')
+    <div class="bg-light p-4 rounded">
+        <h3>Venues</h3>
+        <div class="">
+            <p class="text-muted">Manage your venue here.</p>
+            <a href="{{ route('venue.create') }}" class="btn btn-primary btn-sm float-right">Add venue</a>
+        </div>
+
+        <div class="mt-2">
+            @include('layouts.partials.messages')
+        </div>
 
 
-<div class="bg-light p-4 rounded">
-    <h2>Venues</h2>
-    <div class="lead">
-        Manage your venue here.
-        <a href="{{ route('venue.create') }}" class="btn btn-primary btn-sm float-right">Add venue</a>
+        <div class="row mt-4">
+            @foreach ($venues as $key => $venue)
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center bg-primary text-light"
+                            style="background-image: url({{ url($venue->cover) }}); background-size:cover; background-position:center">
+                            <img src="{{ url($venue->logo) }}" style="width:60px; height: 60px;" class="rounded-circle border border-grey" />
+                            <div class="flex-grow-1 ps-3">
+                                <h5 class="mb-0">{{ $venue->name }}</h5>
+                                <p class="mb-0">{{ $venue->location }}</p>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            {{ $venue->excerpt }}
+                        </div>
+                        <div class="card-footer p-0 d-flex">
+                            <a class="btn btn-default flex-grow-1" href="{{ route('venue.edit', $venue->id) }}">Edit</a>
+                            <div class="vr"></div>
+                            {!! Form::open([
+                                'method' => 'DELETE',
+                                'route' => ['venue.destroy', $venue->id],
+                                'style' => 'display:inline',
+                                'class' => 'flex-grow-1',
+                            ]) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-default  d-block w-100']) !!}
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+
+        <div class="d-flex">
+            {!! $venues->links() !!}
+        </div>
+
     </div>
-
-    <div class="mt-2">
-        @include('layouts.partials.messages')
-    </div>
-
-    <table class="table table-bordered">
-        <tr>
-            <th width="1%">No</th>
-            <th>Name</th>
-            <th>Action</th>
-        </tr>
-        @foreach ($venues as $key => $venue)
-        <tr>
-            <td>{{ $venue->id }}</td>
-            <td>{{ $venue->logo }}</td>
-            <td>{{ $venue->cover }}</td>
-            <td>{{ $venue->name }}</td>
-            {{-- <td>
-                <a class="btn btn-info btn-sm" href="{{ route('venue.show', $venue->id) }}">Show</a>
-            </td> --}}
-            <td>
-                <a class="btn btn-primary btn-sm" href="{{ route('venue.edit', $venue->id) }}">Edit</a>
-            </td>
-            <td>
-                {!! Form::open(['method' => 'DELETE','route' => ['venue.destroy', $venue->id],'style'=>'display:inline']) !!}
-                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                {!! Form::close() !!}
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
-    <div class="d-flex">
-        {!! $venues->links() !!}
-    </div>
-
-</div>
 @endsection
