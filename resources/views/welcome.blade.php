115bd866a81e598b5376ca7c82c078a854d43897
nafishahmeddev <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>
2022-10-01 01:53:25 +0530

    [UPDATE] design fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,5 @@
-@extends('frontend.main.index')
+@extends('frontend.layouts.default')
 
 @section('content')
-
-    @include('frontend.main.header')
-    @include('frontend.main.trending')
-    @include('frontend.main.area')
-    @include('frontend.main.intro')
-    @include('frontend.main.video')
-    @include('frontend.main.top')
-    @include('frontend.main.footer')
-
-   {{-- @include('frontend.top-bar') --}} 
-   {{-- @include('frontend.header') --}} 
-   {{-- @include('frontend.landing.slider') --}} 
-   {{-- @include('frontend.landing.gallery') --}} 
-   {{-- @include('frontend.newsletter') --}} 
-   {{-- @include('frontend.footer') --}} 
 
 @endsection
