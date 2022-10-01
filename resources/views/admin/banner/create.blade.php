8e4c7862d4bdc18f35cfef39d5cbce87216384a1
nafishahmeddev <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>
2022-10-01 16:56:05 +0530

    [UPDATE] design fix

--- resources/views/admin/banner/create.blade.php
+++ resources/views/admin/banner/create.blade.php
@@ -11,6 +11,11 @@
 
             <form method="post" action="{{ route('banner.store') }}">
                 @csrf
+
+
+                <x-image-chooser class="border border-grey mt-5" height="150px" width="150px" :value="null"
+                name="image" />
+
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input value="{{ old('title') }}" 
@@ -38,17 +43,30 @@
                 </div>
 
                 <div class="mb-3">
-                    <label for="body" class="form-label">Body</label>
-                    <textarea class="form-control" 
-                        name="body" 
-                        placeholder="Body" required>{{ old('body') }}</textarea>
+                    <label for="position" class="form-label">Description</label>
+                    <input value="{{ old('position') }}" 
+                        type="text" 
+                        class="form-control" 
+                        name="position" 
+                        placeholder="Position" required>
 
-                    @if ($errors->has('body'))
-                        <span class="text-danger text-left">{{ $errors->first('body') }}</span>
+                    @if ($errors->has('position'))
+                        <span class="text-danger text-left">{{ $errors->first('position') }}</span>
                     @endif
                 </div>
 
+                <div class="mb-3">
+                    <label for="url" class="form-label">Description</label>
+                    <input value="{{ old('url') }}" 
+                        type="text" 
+                        class="form-control" 
+                        name="url" 
+                        placeholder="Url" required>
 
+                    @if ($errors->has('url'))
+                        <span class="text-danger text-left">{{ $errors->first('description') }}</span>
+                    @endif
+                </div>
                 <button type="submit" class="btn btn-primary">Save role</button>
                 <a href="{{ route('banner.index') }}" class="btn btn-default">Back</a>
             </form>
--- resources/views/admin/banner/edit.blade.php
+++ resources/views/admin/banner/edit.blade.php
@@ -12,6 +12,10 @@
             <form method="post" action="{{ route('banner.update', $banner->id) }}">
                 @method('patch')
                 @csrf
+
+                <x-image-chooser class="border border-grey mt-5" height="150px" width="150px" :value="$banner->image"
+                name="image" />
+
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input value="{{ $banner->title }}" 
@@ -39,20 +43,32 @@
                 </div>
 
                 <div class="mb-3">
-                    <label for="body" class="form-label">Body</label>
-                    <textarea
+                    <label for="position" class="form-label">Description</label>
+                    <input value="{{ $banner->position }}" 
                         type="text" 
                         class="form-control" 
-                        name="body" 
-                        placeholder="Body" required>{{ $banner->body }}</textarea>
+                        name="position" 
+                        placeholder="Position" required>
 
-                    @if ($errors->has('body'))
-                        <span class="text-danger text-left">{{ $errors->first('body') }}</span>
+                    @if ($errors->has('position'))
+                        <span class="text-danger text-left">{{ $errors->first('position') }}</span>
                     @endif
                 </div>
 
+                <div class="mb-3">
+                    <label for="url" class="form-label">Description</label>
+                    <input value="{{ $banner->url }}" 
+                        type="text" 
+                        class="form-control" 
+                        name="url" 
+                        placeholder="URL" required>
 
-                <button type="submit" class="btn btn-primary">Save changes</button>
+                    @if ($errors->has('url'))
+                        <span class="text-danger text-left">{{ $errors->first('url') }}</span>
+                    @endif
+                </div>
+
+                     <button type="submit" class="btn btn-primary">Save changes</button>
                 <a href="{{ route('banner.index') }}" class="btn btn-default">Back</a>
             </form>
         </div>
