168a49ddc210ec18e7a7f68cf942ff766085e415
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-10-23 23:43:22 +0530

    [UPDATE] image cache

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -1,29 +1,31 @@
 <footer>
-			<div id="footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12 text-center">
-							<h2>Eksafar Club</h2>
-						</div>
-						<div class="col-md-6 col-md-offset-3 text-center">
-							<p class="fh5co-social-icons">
-								<a href="#"><i class="fas fa-twitter"></i></a>
-								<a href="https://www.facebook.com/eksafar.club"><i class="icon-facebook2"></i></a>
-								<a href="https://www.instagram.com/eksafar.club"><i class="icon-instagram"></i></a>
-								<!-- <a href="#"><i class="icon-dribbble2"></i></a> -->
-								<a href="https://www.youtube.com/channel/UCJZM7qVyoC4unVIuyYZEUcQ"><i class="icon-youtube"></i></a>
-							</p>
-							<p>
-								<a href="{{url('/privacy')}}">Privacy</a> 
-								| 
-								<a href="{{url('/payment-policy')}}">Refund/Cancellation Policy</a> 
-								|
-							
-								<a href="{{url('/terms')}}">Terms and Conditions</a>
-							</p>
-							<p>Copyright 2022 Eksafar Club. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="" target="_blank">Xpeed</a> </p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>Eksafar Club</h2>
+                </div>
+                <div class="col-md-6 col-md-offset-3 text-center">
+                    <p class="fh5co-social-icons">
+                        <a href="#"><i class="fas fa-twitter"></i></a>
+                        <a href="https://www.facebook.com/eksafar.club"><i class="icon-facebook2"></i></a>
+                        <a href="https://www.instagram.com/eksafar.club"><i class="icon-instagram"></i></a>
+                        <!-- <a href="#"><i class="icon-dribbble2"></i></a> -->
+                        <a href="https://www.youtube.com/channel/UCJZM7qVyoC4unVIuyYZEUcQ"><i
+                                class="icon-youtube"></i></a>
+                    </p>
+                    <p>
+                        <a href="{{ url('/privacy') }}">Privacy</a>
+                        |
+                        <a href="{{ url('/payment-policy') }}">Refund/Cancellation Policy</a>
+                        |
+
+                        <a href="{{ url('/terms') }}">Terms and Conditions</a>
+                    </p>
+                    <p>Copyright 2022 Eksafar Club. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a
+                            href="" target="_blank">Xpeed</a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
