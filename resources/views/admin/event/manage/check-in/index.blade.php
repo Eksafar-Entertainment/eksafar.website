305345aab0db4725e41d030324a95e5c68b173fa
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-10-24 01:20:17 +0530

    [UPDATE] code cleanup

--- resources/views/admin/event/manage/check-in/index.blade.php
+++ resources/views/admin/event/manage/check-in/index.blade.php
@@ -82,7 +82,7 @@
         function onScanFailure(error) {
             // handle scan failure, usually better to ignore and keep scanning.
             // for example:
-            console.warn(`Code scan error = ${error}`);
+            //console.warn(`Code scan error = ${error}`);
         }
 
         let html5QrcodeScanner = new Html5QrcodeScanner(
