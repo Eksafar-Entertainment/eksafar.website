7f1fda4234c171563736ee23b4f3e1e9dd9150e9
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-10-23 13:47:45 +0530

    [UPDATE] webhook

--- resources/views/mail/ticket.blade.php
+++ resources/views/mail/ticket.blade.php
@@ -67,7 +67,7 @@
 
             <div class="container">
                 <table style="width: 100%; border-collapse: collapse;">
-                    <tr style="border-bottom: 3px dashed #000000">
+                    <tr style="border-bottom: 2px dashed #000000">
                         <td style="text-align: center; letter-spacing: 1.5; padding: 20px">
                             <img src="{{ url('storage/uploads/qr-' . $order->id . '.png') }}" style="width: 120px"><br>
                             #{{ $order->id }}
