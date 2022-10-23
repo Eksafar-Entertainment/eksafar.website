75a3c26162d4cc809b849d4b4e0176daca6ebd68
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-10-23 16:25:18 +0530

    [UPDATE] webhook

--- resources/views/admin/artist/edit.blade.php
+++ resources/views/admin/artist/edit.blade.php
@@ -1,61 +1,79 @@
 @extends('admin.layouts.admin')
 
 @section('content')
-    <div class="bg-light p-4 rounded">
-        <h2>Update post</h2>
-        <div class="lead">
-            Edit post.
+
+<h4>New Artist</h4>
+<p class="text-muted">Add new post.</p>
+
+<div class="mt-4">
+
+    <form method="POST" action="{{ route('artist.update', $artist->id) }}" enctype="multipart/form-data">
+        @csrf
+        @method('patch')
+        <x-image-chooser class="border border-grey p-4 mb-3" height="auto" width="100%" :value="$artist->cover" name="cover">
+                <x-image-chooser class="border border-grey mt-5" height="150px" width="150px" :value="$artist->image"
+                    name="image" />
+        </x-image-chooser>
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Title</label>
+            <input type="text" class="form-control" name="name" placeholder="Name" value="{{$artist->name}}" required>
+
+            @if ($errors->has('name'))
+            <span class="text-danger text-left">{{ $errors->first('name') }}</span>
+            @endif
         </div>
 
-        <div class="container mt-4">
+        <div class="mb-3">
+            <label for="phone" class="form-label">phone</label>
+            <input type="text" class="form-control" name="phone" placeholder="phone" id="phone" value="{{$artist->phone}}" required>
 
-            <form method="POST" action="{{ route('artist.update', $artist->id) }}">
-                @method('patch')
-                @csrf
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title</label>
-                    <input value="{{ $artist->title }}" 
-                        type="text" 
-                        class="form-control" 
-                        name="title" 
-                        placeholder="Title" required>
-
-                    @if ($errors->has('title'))
-                        <span class="text-danger text-left">{{ $errors->first('title') }}</span>
-                    @endif
-                </div>
-
-                <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <input value="{{ $artist->description }}" 
-                        type="text" 
-                        class="form-control" 
-                        name="description" 
-                        placeholder="Description" required>
-
-                    @if ($errors->has('description'))
-                        <span class="text-danger text-left">{{ $errors->first('description') }}</span>
-                    @endif
-                </div>
-
-                <div class="mb-3">
-                    <label for="body" class="form-label">Body</label>
-                    <textarea
-                        type="text" 
-                        class="form-control" 
-                        name="body" 
-                        placeholder="Body" required>{{ $artist->body }}</textarea>
-
-                    @if ($errors->has('body'))
-                        <span class="text-danger text-left">{{ $errors->first('body') }}</span>
-                    @endif
-                </div>
-
-
-                <button type="submit" class="btn btn-primary">Save changes</button>
-                <a href="{{ route('artist.index') }}" class="btn btn-default">Back</a>
-            </form>
+            @if ($errors->has('phone'))
+            <span class="text-danger text-left">{{ $errors->first('phone') }}</span>
+            @endif
         </div>
 
-    </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">email</label>
+            <input value="{{$artist->email}}" type="text" class="form-control" name="email" placeholder="email" id="email" required>
+
+            @if ($errors->has('email'))
+            <span class="text-danger text-left">{{ $errors->first('email') }}</span>
+            @endif
+        </div>
+
+        <div class="mb-3">
+            <label for="excerpt" class="form-label">excerpt</label>
+            <input value="{{$artist->excerpt}}" type="text" class="form-control" name="excerpt" placeholder="excerpt" id="excerpt" required>
+
+            @if ($errors->has('excerpt'))
+            <span class="text-danger text-left">{{ $errors->first('excerpt') }}</span>
+            @endif
+        </div>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">description</label>
+            <input value="{{$artist->description}}" type="text" class="form-control" name="description" placeholder="description" id="description" required>
+
+            @if ($errors->has('description'))
+            <span class="text-danger text-left">{{ $errors->first('description') }}</span>
+            @endif
+        </div>
+
+        <div class="mb-3">
+            <label for="tags" class="form-label">tags</label>
+            <input value="{{$artist->tags}}" type="text" class="form-control" name="tags" placeholder="tags" id="tags" required>
+
+            @if ($errors->has('tags'))
+            <span class="text-danger text-left">{{ $errors->first('tags') }}</span>
+            @endif
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Save Artist</button>
+        <a href="{{ route('artist.index') }}" class="btn btn-default">Back</a>
+    </form>
+</div>
+
+
 @endsection
