2306402168cdb567fda65dbac439c61e14812afa
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-10-23 19:21:19 +0530

    [UPDATE] webhook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
         <div id="main-carousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 @foreach ($banners as $banner)
-                    <div class="carousel-item active">
-                        <a href="{{$banner->url}}">
-                        <img src="{{$banner->image!=""?url($banner->image):'https://cdn.evbstatic.com/s3-build/fe/build/images/6aaf4a36e35b1b71bc077e200ac7429c-1_tablet_1067x470.jpg'}}" class="w-100" 
-                        style="height: 450px; object-fit:contain; object-position:center" />
+                    <div class="carousel-item active" style="background-image: url({{url($banner->image)}})">
+                        <a href="{{ $banner->url }}" style="backdrop-filter: blur(20px)">
+                            <img src="{{ url($banner->image) }}" class="w-100"
+                                style="height: 450px; object-fit:contain; object-position:center" />
                         </a>
                     </div>
                 @endforeach
@@ -33,7 +33,8 @@
 
             <div class="text-center mb-5">
                 <h1>{{ __('Events') }}</h1>
-                <h5 style="max-width: 500px" class="m-auto fw-normal">Checout The best upcomming events in your city that you won't want to miss...</h5>
+                <h5 style="max-width: 500px" class="m-auto fw-normal">Checout The best upcomming events in your city that
+                    you won't want to miss...</h5>
             </div>
 
             <div class="row gx-5 gy-5">
@@ -43,22 +44,23 @@
 
                             <div>
                                 <a class="" href="{{ url('/event/' . $event->slug) }}">
-                                <div class="w-100 position-relative card border-0"
-                                    style="background-image:url('{{url($event->cover_image)}}'); padding-top: 70%; background-size:cover; background-position:center;">
-                                    
-                                    <div class="position-absolute top-0 start-0 w-100 h-100"
-                                        style="background: linear-gradient(180deg, rgba(0,0,0,0) 54%, rgba(0,0,0,0.7) 83%);  ">
-                                    </div>
-                                 
-                                    <div class="position-absolute bottom-0 start-0 card-body text-light  w-100">
-                                     
+                                    <div class="w-100 position-relative card border-0"
+                                        style="background-image:url('{{ url($event->cover_image) }}'); padding-top: 70%; background-size:cover; background-position:center;">
+
+                                        <div class="position-absolute top-0 start-0 w-100 h-100"
+                                            style="background: linear-gradient(180deg, rgba(0,0,0,0) 54%, rgba(0,0,0,0.7) 83%);  ">
+                                        </div>
+
+                                        <div class="position-absolute bottom-0 start-0 card-body text-light  w-100">
+
                                             <h4>{{ $event->name }}</h4>
                                             <p class=" mb-0">
-                                                {{ \Carbon\Carbon::parse($event->start_date)->format("d-m-Y") }} onwards | Catchup @ Banglore</p>
-                                       
-                                       
+                                                {{ \Carbon\Carbon::parse($event->start_date)->format('d-m-Y') }} onwards |
+                                                Catchup @ Banglore</p>
+
+
+                                        </div>
                                     </div>
-                                </div>
                                 </a>
                             </div>
                         </div>
@@ -74,7 +76,8 @@
 
             <div class="text-center mb-5">
                 <h1>{{ __('Gallery') }}</h1>
-                <h5 style="max-width: 500px" class="m-auto fw-normal">Checkout the Gallery for the past events that have conducted in your city.</h5>
+                <h5 style="max-width: 500px" class="m-auto fw-normal">Checkout the Gallery for the past events that have
+                    conducted in your city.</h5>
             </div>
 
             <div class="row gx-5 gy-5">
@@ -90,4 +93,3 @@
 
     </section>
 @endsection
-
