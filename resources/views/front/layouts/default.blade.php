ebd841d84adb07181635ffa76c6b9ee1df47809d
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-12-10 17:35:37 +0530

    [UPDATE] fix

--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -206,108 +206,12 @@
         </nav>
         <!-- Navbar -->
     </div>
-
-    <!-- <nav class="navbar navbar-expand-md shadow-sm navbar-dark border-bottom">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <img src="{{ url('/images/logo.svg') }}" alt="eksafar-logo" style="height: 25px;" />
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    
-                    <ul class="navbar-nav ms-auto">
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">{{ __('Home') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/gallery">{{ __('Gallery') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">{{ __('Contact') }}</a>
-                        </li>
-                        
-                        @guest
-                                    @if (Route::has('login'))
-    <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                        </li>
-    @endif
-
-                                    @if (Route::has('register'))
-    <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        </li>
-    @endif
-@else
-    <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
-
-                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"
-                                            style="z-index: 9999">
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                            </a>
-
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                        @endguest
-                    </ul>
-                    {{-- <button class="btn btn-primary" id="btnSwitch">Switch</button> --}}
-                </div>
-            </div>
-        </nav> -->
-
     <main>
         @yield('content')
     </main>
 
 
 
-    <!-- <section class="py-5 text-light">
-            <div class="container my-4">
-                <div class="row align-items-center gy-5 gx-5">
-                    <div class="col-md-6">
-                        <div>
-                            <h2>No More Screens. Only LIVE Scenes.</h2>
-                            <p class="mt-4 fs-5">Kiss the couch goodbye and make a checklist of the things you’ve
-                                missed! Concerts.
-                                Comedy. Cricket. Camping. Cool Scenes.</p>
-                            <p class="mt-4 fs-5">Set your destination to: ‘Anywhere, but home.’ Find experiences in &
-                                around your
-                                city
-                                - Step out with the Eksafar Club today.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                    </div>
-                    <div class="col-md-4">
-                        <div>
-                            <img src="{{ url('images/ek.gif') }}"
-                                style="width: 100%; clip-path: polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> -->
 
     <footer class="text-light pt-5 border-top">
 
@@ -367,57 +271,6 @@
             </div>
         </div>
     </footer>
-
-    <!-- <footer class="text-light pt-5 border-top">
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <h4>Useful Links</h4>
-                        <ul class="nav flex-column">
-                            <li class="nav-item"><a class="text-light" href="{{ url('') }}">Home</a></li>
-                            <li class="nav-item"><a class="text-light" href="{{ url('about') }}">About us</a></li>
-                            <li class="nav-item"><a class="text-light" href="{{ url('terms') }}">Terms &
-                                    Condition</a>
-                            </li>
-                            <li class="nav-item"><a class="text-light" href="{{ url('payment-policy') }}">Refund &
-                                    Cancellation Policy</a></li>
-                            <li class="nav-item"><a class="text-light" href="{{ url('privacy') }}">Privacy
-                                    Policy</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <h4>Our Services</h4>
-                        <ul class="nav flex-column">
-                            <li class="nav-item"><a class="text-light" href="#">Events</a></li>
-                            <li class="nav-item"><a class="text-light" href="#">Entertainment</a></li>
-                            <li class="nav-item"><a class="text-light" href="#">Concerts & Comedy</a></li>
-                            <li class="nav-item"><a class="text-light" href="#">Trips & Camping</a></li>
-                            <li class="nav-item"><a class="text-light" href="#">Cool Scenes</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6 footer-contact">
-                        <h4>Contact Us</h4>
-                        <p> HSR, 560087 <br> Bangalore<br> India <br><br> <strong>Phone:</strong>
-                            +91 805 01 49240<br> <strong>Email:</strong> info@eksafar.club<br> </p>
-                    </div>
-                    <div class="col-lg-3 col-md-6 footer-info">
-                        <h3>About Eksafar</h3>
-                        <p>Kiss the couch goodbye and make a checklist of the things you’ve missed! Concerts. Comedy.
-                            Cricket. Camping. Cool Scenes.
-                            Set your destination to: ‘Anywhere, but home.’ Find experiences in & around your city -
-                            Step out with the Eksafar club today..</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container py-3">
-                <div class="copyright"> &copy; Copyright <strong><span>Eksafar</span></strong>. All Rights Reserved
-                </div>
-                <div class="credits"> Designed by <a class="text-light" href="#">xpeed</a> </div>
-            </div>
-        </footer> -->
-    </div>
 </body>
 
 <script>
