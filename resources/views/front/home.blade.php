68bcc302449d3a926ebcc3fae3b931d36f713365
Ravi Sharma <rsravicoder38@gmail.com>
2022-12-11 14:55:03 +0530

    ui changes

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -122,7 +122,7 @@
         </style>
 
         <div class="container">
-            <h2 class="text-center mb-4"><span class="glow">U</span>pdates</h2>
+            <h2 class="text-center mb-4"><span class="glow">U</span>pcoming</h2>
             <div class="owl-carousel owl-theme" id="gallery-carousel">
                 @foreach ($banners as $banner)
                     <div class="item">
@@ -166,7 +166,7 @@
         <div class="container my-5">
 
             <div class="text-center mb-5">
-                <h1><span class="glow">E</span>vents</h1>
+                <h1><span class="glow">A</span>ll Events</h1>
                 <h5 style="max-width: 500px" class="m-auto fw-normal">Checkout The best upcomming events in your city that
                     you won't want to miss...</h5>
             </div>
