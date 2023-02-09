957b4e1b8b532d8d685fd00829d2629f82c33b20
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-02-09 22:22:38 +0530

    jwt

--- resources/views/components/front/navbar.blade.php
+++ resources/views/components/front/navbar.blade.php
@@ -73,7 +73,7 @@
 
 
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="cool" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
