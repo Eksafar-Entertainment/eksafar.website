8ca90a7d95e95549ea3ee4e98e2f8bb5324cb694
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-02-09 22:25:27 +0530

    jwt

--- public/js/front/custom.js
+++ public/js/front/custom.js
@@ -1,5 +1,5 @@
-// theme switch
-document.getElementById('btnSwitch').addEventListener('click',()=>{
-    const mainCss = document.querySelector("#main-css");
-    mainCss.href = mainCss.href.replace("light", "dark");
-})
+// // theme switch
+// document.getElementById('btnSwitch').addEventListener('click',()=>{
+//     const mainCss = document.querySelector("#main-css");
+//     mainCss.href = mainCss.href.replace("light", "dark");
+// })
