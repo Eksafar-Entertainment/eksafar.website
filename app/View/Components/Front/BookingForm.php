b6088bfc7f052c582583ec0ad5b595a3e10f925e
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-02-23 23:50:46 +0530

    featured banner

--- app/View/Components/Front/BookingForm.php
+++ app/View/Components/Front/BookingForm.php
@@ -4,11 +4,12 @@ namespace App\View\Components\Front;
 
 use App\Models\Event;
 use App\Models\Venue;
+use Carbon\Carbon;
 use Illuminate\View\Component;
 
 class BookingForm extends Component
 {
-    public $event, $venue, $event_tickets;
+    public $event, $venue, $event_tickets, $tickets, $dates;
     /**
      * Create a new component instance.
      *
@@ -19,6 +20,14 @@ class BookingForm extends Component
         $this->event = $event;
         $this->venue = $venue;
         $this->event_tickets = $tickets;
+        $dates = [];
+        $ticketss = [];
+        foreach($tickets as $ticket){
+            $dates[] = Carbon::parse($ticket->start_datetime)->format("Y-m-d H:i").":00";
+            $ticketss[Carbon::parse($ticket->start_datetime)->format("Y-m-d H:i").":00"][] = $ticket;
+        }
+        $this->dates = array_unique($dates);
+        $this->tickets = $ticketss;
     }
 
     /**
--- resources/views/components/front/booking-form.blade.php
+++ resources/views/components/front/booking-form.blade.php
@@ -9,54 +9,69 @@
                 <div class="h-100 p-4 ">
                     <div>
                         <h4 class="mb-0">{{ $event->name }} @ {{ $venue->name }}</h4>
-                        <small class="text-muted">
-                            <i class="fas fa-calendar me-2"></i>
-                            {{ \Carbon\Carbon::parse($event_tickets[0]->start_datetime)->format('d-M-Y | h:i A') }} Onwards
-                        </small>
                     </div>
-                    <div class="table-responsive-sm">
-                        <table class="table table-ms mt-3">
-                            @foreach ($event_tickets as $n => $event_ticket)
-                                <tr data-row="ticket">
-                                    <td width="50%" class="ps-0">
-                                        <h6 class="mb-0 text-light">{{ $event_ticket->name }}
-                                            <br/>
-                                            <span class="badge bg-danger">
-                                                {{ \Carbon\Carbon::parse($event_ticket->start_datetime)->format('d-m-Y') }}
-                                            </span>
-                                        </h6>
-                                        <small class="text-muted">{{ $event_ticket->description }}</small><br />
-                                        <span class="text-light"> @money($event_ticket->price)</span>
-                                    </td>
-                                    <td class="align-middle pe-0 pr-0" width="1%">
-                                        <input type="hidden" name="items[{{ $n }}][event_ticket_id]"
-                                            value="{{ $event_ticket->id }}" class="" />
-                                        <input type="hidden" value="{{ $event_ticket->price }}" data-field="price" />
-                                        <div class="input-group" style="width: 100px;transform: scale(0.8)">
-                                            <button type="button" class="btn btn-danger btn-number btn-sm"
-                                                data-field="minus">
-                                                <span class="fa fa-minus"></span>
-                                            </button>
-                                            <input type="number" class="form-control form-control-sm bare text-center"
-                                                data-field='quantity' name="items[{{ $n }}][quantity]"
-                                                value="0" @if ($event_ticket->status == 'SOLD') disabled @endif />
-                                            <button type="button" class=" btn btn-danger btn-number btn-sm"
-                                                data-field="plus">
-                                                <span class="fa fa-plus"></span>
-                                            </button>
-                                        </div>
-                                    </td>
-                                    <td class="fs-6 align-middle pe-0 text-nowrap text-end text-light d-sm-table-cell d-none"
-                                        width="1%" style="min-width: 60px" data-field="total-price">
-                                        @if ($event_ticket->status == 'SOLD')
-                                            <span class="badge bg-danger"> Sold Out</span>
-                                        @else
-                                            <span class="text-light">@money(0)</span>
-                                        @endif
-                                    </td>
-                                </tr>
+                    <nav>
+                        <div class="nav nav-tabs" id="dates-tab" role="tablist">
+                            @foreach ($dates as $i => $date)
+                                <button class="nav-link {{ $i == 0 ? 'active' : '' }} text-white"
+                                    id="nav-{{ $i }}-tab" data-bs-toggle="tab"
+                                    data-bs-target="#nav-{{ $i }}" type="button" role="tab"
+                                    aria-controls="nav-{{ $i }}"
+                                    aria-selected="{{ $i == 0 ? 'true' : 'false' }}"
+                                    onclick="resetForm()">{{ \Carbon\Carbon::parse($date)->format("dS M, Y") }}</button>
                             @endforeach
-                        </table>
+                        </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+                        @foreach ($dates as $i => $date)
+                            <div class="tab-pane fade {{ $i == 0 ? 'active show' : '' }}" id="nav-{{ $i }}"
+                                role="tabpanel" aria-labelledby="nav-{{ $i }}-tab">
+                                <div class="table-responsive-sm">
+                                    <table class="table table-ms mt-3">
+                                        @foreach ($event_tickets as $n => $event_ticket)
+                                            <tr data-row="ticket">
+                                                <td width="50%" class="ps-0">
+                                                    <h6 class="mb-0 text-light">{{ $event_ticket->name }}</h6>
+                                                    <small class="text-muted"
+                                                        style="font-size:10px; line-height:0.5 !important">{{ $event_ticket->description }}</small><br />
+                                                    <span class="text-light"> @money($event_ticket->price)</span>
+                                                </td>
+                                                <td class="align-middle pe-0 pr-0" width="1%">
+                                                    <input type="hidden"
+                                                        name="items[{{ $n }}][event_ticket_id]"
+                                                        value="{{ $event_ticket->id }}" class="" />
+                                                    <input type="hidden" value="{{ $event_ticket->price }}"
+                                                        data-field="price" />
+                                                    <div class="input-group" style="width: 100px;transform: scale(0.8)">
+                                                        <button type="button" class="btn btn-danger btn-number btn-sm"
+                                                            data-field="minus">
+                                                            <span class="fa fa-minus"></span>
+                                                        </button>
+                                                        <input type="number"
+                                                            class="form-control form-control-sm bare text-center"
+                                                            data-field='quantity'
+                                                            name="items[{{ $n }}][quantity]" value="0"
+                                                            @if ($event_ticket->status == 'SOLD') disabled @endif />
+                                                        <button type="button" class=" btn btn-danger btn-number btn-sm"
+                                                            data-field="plus">
+                                                            <span class="fa fa-plus"></span>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                                <td class="fs-6 align-middle pe-0 text-nowrap text-end text-light d-sm-table-cell d-none"
+                                                    width="1%" style="min-width: 60px" data-field="total-price">
+                                                    @if ($event_ticket->status == 'SOLD')
+                                                        <span class="badge bg-danger"> Sold Out</span>
+                                                    @else
+                                                        <span class="text-light">@money(0)</span>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -101,48 +116,6 @@
                                 class="form-control" />
                             <button class="btn btn-danger" type="button"> Redeem </button>
                         </div>
-                        {{-- @guest
-                            <div>
-                                <hr />
-                                <!-- Email Container -->
-                                <div id="email-container">
-                                    <div class="mb-3">
-                                        <input type="email" id="email" name="email" placeholder="Email"
-                                            class="form-control" />
-                                    </div>
-                                    <button class="btn btn-primary w-100" type="button">Continue</button>
-                                </div>
-                                <!-- login Container -->
-                                <div id="login-container" style="display: none">
-                                    <div class="mb-3" class="password">
-                                        <input type="password" placeholder="Password" name="password"
-                                            class="form-control" />
-                                    </div>
-                                        @if (Route::has('password.request'))
-                                            <a class="btn btn-link" style="color:lightgrey" target="_blank" href="{{ route('password.request') }}">
-                                                {{ __('Forgot Your Password?') }}
-                                            </a>
-                                        @endif
-                                    <button class="btn btn-primary w-100" type="button">Login</button>
-                                </div>
-                                <!-- register Container -->
-                                <div id="register-container" style="display: none">
-                                    <div class="mb-3" class="name">
-                                        <input type="text" placeholder="Full name" class="form-control"
-                                            name="name" />
-                                    </div>
-                                    <div class="mb-3" class="mobile">
-                                        <input type="number" placeholder="Phone" name="mobile" class="form-control" />
-                                    </div>
-
-                                    <div class="mb-3" class="password">
-                                        <input type="password" placeholder="Password" name="password"
-                                            class="form-control" />
-                                    </div>
-                                    <button class="btn btn-primary w-100" type="button">Register</button>
-                                </div>
-                            </div>
-                        @endguest --}}
                         <hr />
                         <div>
                             <div class="mb-3">
@@ -224,6 +197,22 @@
             _evt.preventDefault();
             alert("Please select a ticket");
         }
+    }
+
+    function resetForm() {
+        document.querySelectorAll("[data-row=ticket]").forEach(_row => {
+            const _quantity_field = _row.querySelector("[data-field=quantity]");
+            const _price_field = _row.querySelector("[data-field=price]");
+            const _total_price_field = _row.querySelector("[data-field=total-price]");
+            const _minus_btn = _row.querySelector("[data-field=minus]");
+            const _plus_btn = _row.querySelector("[data-field=plus]");
+
+            _quantity_field.value = 0;
+            _total_price_field.innerHTML = money(0);
+            _total_price_field.setAttribute("amount", 0);
+
+        });
+        calculateSummery();
     }
 
 
@@ -384,3 +373,21 @@
 
     })
 </script>
+
+
+<style>
+    #dates-tab {
+        margin-top: 15px
+    }
+
+    #dates-tab .nav-link {
+        font-size: 12px;
+        color: white;
+        padding: 2px 5px
+    }
+
+    #dates-tab .nav-link.active {
+        background-color: white;
+        color: var(--bs-primary) !important;
+    }
+</style>
