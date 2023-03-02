dce2e5b2a5c7e8f9f15fb531b2b287aecaadee94
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-03-02 20:49:43 +0530

    trigger email

--- resources/views/front/payment/razorpay/checkout.blade.php
+++ resources/views/front/payment/razorpay/checkout.blade.php
@@ -1,25 +1,32 @@
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 <script>
     var options = {
-        "key": "{{$key}}", // Enter the Key ID generated from the Dashboard
-        "amount": "{{$order_details->amount}}", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-        "currency": "{{$order_details->currency}}",
+        "key": "{{ $key }}", // Enter the Key ID generated from the Dashboard
+        "amount": "{{ $order_details->amount }}", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "currency": "{{ $order_details->currency }}",
         "name": "Eksafar Entertainment",
-        "description": "{{$event->name}}",
-        "image": "{{url('images/ek-logo.png')}}",
-        "order_id": "{{$order_details->id}}", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "description": "{{ $event->name }}",
+        "image": "{{ url('images/ek-logo.png') }}",
+        "order_id": "{{ $order_details->id }}", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
         "redirect": true,
-        "callback_url": "{{url('payment/razorpay/complete')}}",
+        "callback_url": "{{ url('payment/razorpay/complete') }}",
         "prefill": {
-            "name": "{{$customer_details['name']}}",
-            "email": "{{$customer_details['email']}}",
-            "contact": "{{$customer_details['mobile']}}"
+            "name": "{{ $customer_details['name'] }}",
+            "email": "{{ $customer_details['email'] }}",
+            "contact": "{{ $customer_details['mobile'] }}"
         },
         "notes": {
 
         },
         "theme": {
             "color": "#3399cc"
+        },
+        "modal": {
+            "ondismiss": function() {
+                console.log('Checkout form closed');
+                alert("Payment cancelled");
+                window.location.href = "{{ url('/event/'.$event->slug) }}";
+            }
         }
     };
     var rzp1 = new Razorpay(options);
@@ -31,7 +38,7 @@
         //alert(response.error.reason);
         //alert(response.error.metadata.order_id);
         //alert(response.error.metadata.payment_id);
-        window.location.href = "{{url('/')}}"
+        window.location.href = "{{ url('/event/'.$event->slug) }}";
     });
     rzp1.open();
 </script>
