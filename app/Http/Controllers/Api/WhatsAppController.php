618157645f078eab0638870332c77cd10e770241
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-03-02 23:37:45 +0530

    trigger email

--- app/Http/Controllers/Api/WhatsAppController.php
+++ app/Http/Controllers/Api/WhatsAppController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 
@@ -26,32 +27,35 @@ class WhatsAppController extends Controller
         Log::channel('whatsapp-notification')->info(json_encode($data, JSON_PRETTY_PRINT));
         try {
             $entries = $data->entry;
-            $text = "Message from whatsapp" . PHP_EOL;
+            $text = "Message from whatsapp\n";
             $has_message = false;
             foreach ($entries as $entry) {
                 $changes = $entry->changes;
                 foreach ($changes as $change) {
                     $messages = $change->value->messages;
                     foreach ($messages as $message) {
-                        $entity = "From : " . $message->from . PHP_EOL;
+                        $entity = "From : <a href='https://wa.me/".$message->from."'>".$message->from."</a>\n";
                         if ($message->type == "text") {
-                            $entity .= "" . $message->text->body . PHP_EOL;
+                            $entity .= "<strong>Text : " .$message->text->body."</strong>\n";
                         } else {
-                            $entity .= $message->type . " message" . PHP_EOL;
+                            $entity .= "<i>" .$message->type . " message" ."</i>\n";
                         }
-                        $text .= $entity . PHP_EOL;
+                        $text .= $entity;
                         $has_message = true;
                     }
                 }
             }
+          
             if ($has_message) {
-                Mail::raw($text . PHP_EOL . PHP_EOL . PHP_EOL . "<pre>" . json_encode($data, JSON_PRETTY_PRINT) . "</pre>", function ($message) {
-                    $message->to("support@eksafar.club")
-                        ->subject("Message from whatsapp");
-                });
+                Http::post("https://api.telegram.org/bot" . env("TELEGRAM_BOT_TOKEN") . "/sendMessage", [
+                    "chat_id" => env("TELEGRAM_CHAT_ID"),
+                    "text" => $text,
+                    "parse_mode" => "HTML"
+                ]);
             }
+            dd($text);
         } catch (Exception $err) {
-            print "Error". $err->getMessage();
+            print "Error" . $err->getMessage();
         }
 
 
