3a81f0441c1c644c33091dc06d15720d4adb6100
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-03-04 17:12:22 +0530

    fix

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -24,15 +24,16 @@
         <div class="overflow-auto card rounded mt-4">
             <table class="table table-striped bg-white mb-0">
                 <tr>
-                    <th>No</th>
+                    <th>#</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Roles</th>
-                    <th colspan="3">Action</th>
+                    <th></th>
                 </tr>
                 @foreach ($data as $key => $admin)
                     <tr>
                         <td width="1%">{{ ++$i }}</td>
+                       
                         <td>{{ $admin->name }}</td>
                         <td>{{ $admin->email }}</td>
                         <td>
@@ -42,19 +43,15 @@
                                 @endforeach
                             @endif
                         </td>
-                        <td width="1%">
-                            <a class="btn btn-sm btn-info" href="{{ route('admins.show', $admin->id) }}"><i
-                                    class="fas fa-eye"></i></a>
-                        </td>
-                        <td width="1%">
-                            <a class="btn btn-sm btn-primary" href="{{ route('admins.edit', $admin->id) }}"><i
-                                    class="fas fa-pencil"></i></a>
-                        </td>
-                        <td width="1%">
-                            {!! Form::open(['method' => 'DELETE', 'route' => ['admins.destroy', $admin->id], 'style' => 'display:inline']) !!}
-                            {!! Form::button('<i class="fas fa-trash"></i>', ['class' => 'btn btn-sm btn-danger', 'type' => 'submit']) !!}
+                        <td width='1%' nowrap>
+                            <a href="{{ route('admins.show', $admin->id) }}" class="mx-1"><i class="fas fa-eye"></i></a>
+                            <a href="{{ route('admins.edit', $admin->id) }}" class="mx-1"><i class="fas fa-pencil"></i></a>
+
+                            {!! Form::open(['method' => 'DELETE', 'route' => ['admins.destroy', $admin->id], 'style' => 'display:inline', 'onclick'=>'if(!confirm("Are you sure?")){event.preventDefault();};', 'class'=>'mx-1']) !!}
+                            {!! Form::button('<i class="fas fa-trash"></i>', ['class' => 'border-0 bg-transparent text-danger', 'type' => 'submit']) !!}
                             {!! Form::close() !!}
                         </td>
+                   
                     </tr>
                 @endforeach
             </table>
