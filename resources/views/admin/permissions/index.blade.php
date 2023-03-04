a3472b0006a2d2bba4fd0e9eb34f50daa09f7743
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-03-04 17:01:22 +0530

    fix

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,36 +1,44 @@
 @extends('admin.layouts.admin')
 
 @section('content')
+    <div>
+        <div class="row">
+            <div class="col">
+                <h3>Permissions</h3>
 
-    <h1 class="mb-3">Eksafar Entertainment</h1>
-
-    <div class="bg-light p-4 rounded">
-        <h2>Permissions</h2>
-        <div class="lead">
-            Manage your permissions here.
-            <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right">Add permissions</a>
+                Manage your permissions here.
+            </div>
+            <div class="col-auto">
+            <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm">Add permissions</a>
+            </div>
         </div>
 
         <div class="mt-2">
             @include('admin.layouts.partials.messages')
         </div>
 
-        <table class="table table-striped">
+        <div class="card">
+        <table class="table table-striped ">
             <thead>
-            <tr>
-                <th scope="col" width="15%">Name</th>
-                <th scope="col">Guard</th> 
-                <th scope="col" colspan="3" width="1%"></th> 
-            </tr>
+                <tr>
+                    <th scope="col" width="15%">Name</th>
+                    <th scope="col">Guard</th>
+                    <th scope="col" colspan="3" width="1%"></th>
+                </tr>
             </thead>
             <tbody>
-                @foreach($permissions as $permission)
+                @foreach ($permissions as $permission)
                     <tr>
                         <td>{{ $permission->name }}</td>
                         <td>{{ $permission->guard_name }}</td>
-                        <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                        <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">Edit</a>
+                        </td>
                         <td>
-                            {!! Form::open(['method' => 'DELETE','route' => ['permissions.destroy', $permission->id],'style'=>'display:inline']) !!}
+                            {!! Form::open([
+                                'method' => 'DELETE',
+                                'route' => ['permissions.destroy', $permission->id],
+                                'style' => 'display:inline',
+                            ]) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         </td>
@@ -38,6 +46,7 @@
                 @endforeach
             </tbody>
         </table>
+        </div>
 
     </div>
 @endsection
